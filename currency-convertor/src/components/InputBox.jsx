f2f18bfbc4aa8e3bc859e04103f7cd2ecb347c26
Shivam Kashyap <shivam.kashyap@thrymr.net>
2024-02-23 10:16:03 +0530

    Worked on the currency convertor project

--- currency-convertor/src/components/InputBox.jsx
+++ currency-convertor/src/components/InputBox.jsx
@@ -1,3 +1,5 @@
+import { useId } from "react";
+
 function InputBox({
   label,
   amount,
@@ -9,12 +11,16 @@ function InputBox({
   currencyDisable = false,
   className = "",
 }) {
+  const id = useId();
   return (
     <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
       <div className="w-1/2">
-        <label className="text-black/40 mb-2 inline-block">{label}</label>
+        <label htmlFor={id} className="text-black/40 mb-2 inline-block">
+          {label}
+        </label>
         <input
           type="number"
+          id={id}
           className="outline-none w-full
                        bg-transparent py-1.5"
           placeholder="Amount"
--- currency-convertor/src/hooks/useCurrencyInfo.js
+++ currency-convertor/src/hooks/useCurrencyInfo.js
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react";
 // eslint-disable-next-line react-hooks/rules-of-hooks
-const [data, setdata] = useState({});
 
 function useCurrencyInfo(currency) {
+  const [data, setData] = useState(/*  */ {});
   useEffect(() => {
     fetch(
       `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
     )
       .then((res) => res.json())
-      .then((res) => setdata(res[currency]));
+      .then((res) => setData(res[currency]));
     console.log(data);
   }, [currency]);
   console.log(data);
